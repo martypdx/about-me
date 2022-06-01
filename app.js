77ee0d7babfbcb64bed83911ca17d6c8676acd16
Marty Nelson <marty@martypdx.com>
2022-06-01 09:43:28 -0700

    working example

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 // grab DOM elements
+const tosCheckbox = document.getElementById('tos-checkbox');
+const showAnswerButton = document.getElementById('show-answer-button');
+const animalSection = document.getElementById('animal-section');
 
-// set event listeners
-    // get info from user input
-    // use user input to update state
-    // update DOM to reflect the new state
+const nameInput = document.getElementById('name-input');
+const nameDisplay = document.getElementById('name-display');
+const imageSelect = document.getElementById('image-select');
+const imageDisplay = document.getElementById('image-display');
+
+tosCheckbox.addEventListener('change', () => {
+    showAnswerButton.disabled = !tosCheckbox.checked;
+});
+
+showAnswerButton.addEventListener('click', () => {
+    animalSection.classList.remove('hidden');
+});
+
+nameInput.addEventListener('input', () => {
+    nameDisplay.textContent = nameInput.value;
+});
+
+imageSelect.addEventListener('change', () => {
+    const src = 'assets/cats/' + imageSelect.value;
+    imageDisplay.src = src;
+});
